97d7e108fc78e5793083086dac6cd6ddab921da3
mumushuiding <181457121@qq.com>
2020-11-12 09:14:52 +0800

    commit

--- lib/widget/button/button_countdown.dart
+++ lib/widget/button/button_countdown.dart
@@ -1,63 +1,66 @@
 import 'dart:async';
-
 import 'package:flutter/material.dart';
 
-class ButtonCountDown extends StatefulWidget{
+class ButtonCountDown extends StatefulWidget {
   final int seconds;
   final callback;
   final String text;
   final double width;
   final double elevation;
-  ButtonCountDown({Key key,this.seconds,this.callback,this.text,this.width,this.elevation}):super(key:key);
+  ButtonCountDown({Key key, this.seconds, this.callback, this.text, this.width, this.elevation}) : super(key: key);
   @override
   State<StatefulWidget> createState() {
     return _ButtonCountDownState();
   }
-
 }
-class _ButtonCountDownState extends State<ButtonCountDown>{
+
+class _ButtonCountDownState extends State<ButtonCountDown> {
   int _seconds;
   Timer _timer;
   double _width;
   String _text;
   Function _onPress;
 
-  @override void initState(){
+  @override
+  void initState() {
     super.initState();
-    _width=widget.width;
-    _text=widget.text;
-    if (_text==null){
-      _text='确定';
+    _width = widget.width;
+    _text = widget.text;
+    if (_text == null) {
+      _text = '确定';
     }
     setSeconds();
-    _onPress=onPress;
+    _onPress = onPress;
   }
-  void setSeconds(){
-    _seconds=widget.seconds;
-    if (_seconds==null) _seconds=3;
+
+  void setSeconds() {
+    _seconds = widget.seconds;
+    if (_seconds == null) _seconds = 3;
   }
-  Function onPress(){
-    return (){
-      if (_timer!=null)return;
+
+  Function onPress() {
+    return () {
+      if (_timer != null) return;
       _startTimer();
-      if (widget.callback!=null){
-            widget.callback();
-          }
+      if (widget.callback != null) {
+        widget.callback();
+      }
     }();
   }
-  void _startTimer(){
-    _onPress=null;
-    _text=_seconds.toString();
+
+  void _startTimer() {
+    _onPress = null;
+    _text = _seconds.toString();
     setState(() {});
-    _timer=Timer.periodic(Duration(seconds: 1),(timer){
-      _text=_seconds.toString();
-      if (_seconds<=0){
+    _timer = Timer.periodic(Duration(seconds: 1), (timer) {
+      _text = _seconds.toString();
+      if (_seconds <= 0) {
         _cancelTimer();
-        _onPress=onPress;
+        _onPress = onPress;
         setSeconds();
-        _text=widget.text;
-        if (_text==null){
-          _text='确定';
+        _text = widget.text;
+        if (_text == null) {
+          _text = '确定';
         }
         setState(() {});
         return;
@@ -66,21 +69,22 @@ class _ButtonCountDownState extends State<ButtonCountDown>{
       _seconds--;
     });
   }
-  void _cancelTimer(){
+
+  void _cancelTimer() {
     _timer?.cancel();
-    _timer=null;
+    _timer = null;
   }
+
   @override
   Widget build(BuildContext context) {
     return MaterialButton(
-        color: Colors.blue,
-        child: Text(_text),
-        disabledColor: Colors.grey,
-        minWidth: _width,
-        elevation: widget.elevation,
-        disabledElevation: 0,
-        onPressed: _onPress,
+      color: Colors.blue,
+      child: Text(_text),
+      disabledColor: Colors.grey,
+      minWidth: _width,
+      elevation: widget.elevation,
+      disabledElevation: 0,
+      onPressed: _onPress,
     );
   }
-  
 }
